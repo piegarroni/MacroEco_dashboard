d4614fc187059ebeae6acbdb049d5ae47fd3f1aa
piegarroni <pietrogarroni@hotmail.com>
2023-03-01 15:10:05 +0100

    formatting and comments

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output, ctx  # pip install dash (version 2.0.0 or higher)
+from dash import (
+    Dash,
+    dcc,
+    html,
+    Input,
+    Output,
+    ctx,
+)  # pip install dash (version 2.0.0 or higher)
 from datetime import date
 from sklearn import preprocessing
 import dash_bootstrap_components as dbc
@@ -13,36 +20,34 @@ import os
 external_stylesheets = [dbc.themes.SLATE]
 
 # dash app with external stylesheet
-app = Dash(__name__,
-           external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-# Import and clean data 
+# Import and clean data
 try:
     my_instance = fred_scraper.fred_retriever()
-    df = my_instance.merge()
-    df.to_csv(os.getcwd() + '/data.csv')
+    df = my_instance.run()
+    df.to_csv(os.getcwd() + "/data.csv")
     index = df.index
 except:
     print("failed to download data")
     df = pd.read_csv("data.csv")
-    df['DATE'] = pd.to_datetime(df['DATE'], utc=True)
-    index = df['DATE'] 
-    df = df.set_index('DATE')
+    df["DATE"] = pd.to_datetime(df["DATE"], utc=True)
+    index = df["DATE"]
+    df = df.set_index("DATE")
 
-"""
-def scaler(df):
-    x = df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    df = pd.DataFrame(x_scaled, columns = df.columns).set_index(df.index)
-    return df
 
-df = scaler(df)
-df = df.set_index(index)
-"""
 # bottoms economy
-bottoms = ['04-01-1975', '12-01-1982', '03-01-1992', '08-01-2002', '03-01-2009', '05-01-2020', '02-01-2021', str(date.today())]
+bottoms = [
+    "04-01-1975",
+    "12-01-1982",
+    "03-01-1992",
+    "08-01-2002",
+    "03-01-2009",
+    "05-01-2020",
+    "02-01-2021",
+    str(date.today()),
+]
 
 # days ranges
 days = [i for i in range(0, 13574)]
@@ -50,131 +55,148 @@ df_dates = [i.date() for i in df.index]
 
 # ------------------------------------------------------------------------------
 # App layout
-app.layout = html.Div([
-    
-
-        html.H1("Visualization Tool for History Economy", style={'text-align': 'center'}),
-
-        dcc.Checklist(id = "select_variables",
+app.layout = html.Div(
+    [
+        html.H1(
+            "Visualization Tool for History Economy", style={"text-align": "center"}
+        ),
+        dcc.Checklist(
+            id="select_variables",
             options=[
-                {'label': '  Interest rates  ', 'value': 'DFF'},
-                {'label': '  Growth  ', 'value': 'ECOGROWTH'},
-                {'label': '  Inflation  ', 'value': 'INFLATION'},
-                {'label': '  Nasdaqcom  ', 'value': 'NASDAQCOM'},
-                {'label': '  Nyse ', 'value': 'BOGZ1FL073164003Q'},
-                {'label': '  Willshire ', 'value': 'WILL5000INDFC'},
-                {'label': '  5y Breakeven Inflation', 'value': 'T5YIE'},
+                {"label": "  Interest rates  ", "value": "DFF"},
+                {"label": "  Growth  ", "value": "ECOGROWTH"},
+                {"label": "  Inflation  ", "value": "INFLATION"},
+                {"label": "  Nasdaqcom  ", "value": "NASDAQCOM"},
+                {"label": "  Nyse ", "value": "BOGZ1FL073164003Q"},
+                {"label": "  Willshire ", "value": "WILL5000INDFC"},
+                {"label": "  5y Breakeven Inflation", "value": "T5YIE"},
             ],
-            value=['ECOGROWTH'],
-            labelStyle={'fontSize': '20px', 'margin-right': '5px'}
-        ),
-
-        html.Br(),
-
-        dcc.Graph(id='history_plot', figure={}, style={'width': '100%', 'height': '67vh', 'backgroundColor': 'black'}),
-        html.Br(),
-        html.Br(),
-        html.H1("Visualization Tool for Past Economic Cycles", style={'text-align': 'center'}),
-        html.Br(),
-        dcc.Dropdown(id="select_cycle",
-                    options=[
-                        {"label": "1975-1982", "value": 0},
-                        {"label": "1982-1992", "value": 1},
-                        {"label": "1992-2002", "value": 2},
-                        {"label": "2002-2009", "value": 3},
-                        {"label": "2009-2020", "value": 4}],
-                    multi=False,
-                    value=4,
-                    style={'width': "40%", "fontsize": "25px"}
+            value=["ECOGROWTH"],
+            labelStyle={"fontSize": "20px", "margin-right": "5px"},
         ),
         html.Br(),
-        dcc.Graph(id='compare_plot', figure={}, style={'width': '100%', 'height': '67vh'}), 
+        dcc.Graph(
+            id="history_plot",
+            figure={},
+            style={"width": "100%", "height": "67vh", "backgroundColor": "black"},
+        ),
         html.Br(),
         html.Br(),
-
-        
-        html.H1("Comparison with slider", style={'text-align': 'center'}),
+        html.H1(
+            "Visualization Tool for Past Economic Cycles",
+            style={"text-align": "center"},
+        ),
+        html.Br(),
+        dcc.Dropdown(
+            id="select_cycle",
+            options=[
+                {"label": "1975-1982", "value": 0},
+                {"label": "1982-1992", "value": 1},
+                {"label": "1992-2002", "value": 2},
+                {"label": "2002-2009", "value": 3},
+                {"label": "2009-2020", "value": 4},
+            ],
+            multi=False,
+            value=4,
+            style={"width": "40%", "fontsize": "25px"},
+        ),
+        html.Br(),
+        dcc.Graph(
+            id="compare_plot", figure={}, style={"width": "100%", "height": "67vh"}
+        ),
+        html.Br(),
+        html.Br(),
+        html.H1("Comparison with slider", style={"text-align": "center"}),
         html.Br(),
         html.Div(
-            dcc.RangeSlider(id='select_year',
-                            marks={i: {"label": "   " + str(df_dates[i]), "style": {"transform": "rotate(45deg)", 'color':'white'}}
-                                        for i in range(0, len(days), 200)},
-                            min=0,
-                            max=len(days) - 1,
-                            value=[0, len(days) - 1],
+            dcc.RangeSlider(
+                id="select_year",
+                marks={
+                    i: {
+                        "label": "   " + str(df_dates[i]),
+                        "style": {"transform": "rotate(45deg)", "color": "white"},
+                    }
+                    for i in range(0, len(days), 200)
+                },
+                min=0,
+                max=len(days) - 1,
+                value=[0, len(days) - 1],
             ),
-            style={"margin-left": "50px", "margin-right": "50px", "margin-top": "25px", "margin-bottom": "25px"} 
+            style={
+                "margin-left": "50px",
+                "margin-right": "50px",
+                "margin-top": "25px",
+                "margin-bottom": "25px",
+            },
         ),
-
         html.Br(),
-        html.Div(id='output'),
+        html.Div(id="output"),
         html.Br(),
-        dcc.Graph(id='compare_slider_plot', figure={}, style={'width': '100%', 'height': '67vh'}), 
+        dcc.Graph(
+            id="compare_slider_plot",
+            figure={},
+            style={"width": "100%", "height": "67vh"},
+        ),
         html.Br(),
         html.Br(),
-
-
-        html.Div(id='output_container', children=[]),
+        html.Div(id="output_container", children=[]),
     ],
-    style={"margin": "35px 5% 75px 5%"}
+    style={"margin": "35px 5% 75px 5%"},
 )
 
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='history_plot', component_property='figure'),
-     Output(component_id='compare_plot', component_property='figure'),
-     Output(component_id='compare_slider_plot', component_property='figure')],
-
-    [Input(component_id='select_variables', component_property='value'),
-     Input(component_id='select_cycle', component_property='value'),
-     Input(component_id='select_year', component_property='value')]
+    [
+        Output(component_id="output_container", component_property="children"),
+        Output(component_id="history_plot", component_property="figure"),
+        Output(component_id="compare_plot", component_property="figure"),
+        Output(component_id="compare_slider_plot", component_property="figure"),
+    ],
+    [
+        Input(component_id="select_variables", component_property="value"),
+        Input(component_id="select_cycle", component_property="value"),
+        Input(component_id="select_year", component_property="value"),
+    ],
 )
-
-
 def update_graph(variables, cycle, slider):
-    # graphs layout
+    # Define graphs layout
     layout = go.Layout(
-        title={
-            'text': 'Economy history',
-            'font': {'size': 24}
-        },
-        xaxis={
-            'title': 'Date',
-            'titlefont': {'size': 20},
-            'tickfont': {'size': 16}
-        },
+        title={"text": "Economy history", "font": {"size": 24}},
+        xaxis={"title": "Date", "titlefont": {"size": 20}, "tickfont": {"size": 16}},
         yaxis={
-            'title': 'Scaled Value',
-            'titlefont': {'size': 20},
-            'tickfont': {'size': 16}
-        }, 
-        plot_bgcolor='#d7d9e0'
+            "title": "Scaled Value",
+            "titlefont": {"size": 20},
+            "tickfont": {"size": 16},
+        },
+        plot_bgcolor="#d7d9e0",
     )
 
     # print variables selection
     container = f"The variables chosen by user was: {variables}, the cycle chosen was {bottoms[cycle]} to {bottoms[cycle+1]}"
 
-    #--------------------------------
+    # --------------------------------
     # first graph: historical data
     dff = df.copy()
     hist = go.Figure(layout=layout)
     for column in variables:
-        #smoothen
+        # smoothen
         dff[column] = dff[column].rolling(window=45).mean()
         # Plotly Express
-        hist.add_trace(go.Scatter(x=dff.index, y=dff[column], mode='lines', name=column, opacity=0.7))
-        
+        hist.add_trace(
+            go.Scatter(
+                x=dff.index, y=dff[column], mode="lines", name=column, opacity=0.7
+            )
+        )
 
-    #-------------------------------
+    # -------------------------------
     # second graph: cycles visualization and comparison
 
     dff = df.copy()
 
     # loc data on right cycle
-    past_cycle = dff.loc[(dff.index >  bottoms[cycle]) & (df.index <  bottoms[cycle+1])]
+    past_cycle = dff.loc[(dff.index > bottoms[cycle]) & (df.index < bottoms[cycle + 1])]
     present_cycle = dff.loc[(dff.index > bottoms[-2])]
     index = [i for i in past_cycle.index]
 
@@ -190,25 +212,44 @@ def update_graph(variables, cycle, slider):
         present_cycle[column] = present_cycle[column].rolling(window=45).mean()
 
         # Plotly Express
-        comparison.add_trace(go.Scatter(x=index, y=past_cycle[column], mode='lines', name=f'{column} past', opacity=0.7))
-        comparison.add_trace(go.Scatter(x=index, y=present_cycle[column], mode='lines', name=f'{column}  present', opacity=0.7))
- 
-    # update layout with new title
-    comparison.update_layout(title={
-            'text': 'Economic cycles comparison (present vs past)',
-            'font': {'size': 24}})
-    
+        comparison.add_trace(
+            go.Scatter(
+                x=index,
+                y=past_cycle[column],
+                mode="lines",
+                name=f"{column} past",
+                opacity=0.7,
+            )
+        )
+        comparison.add_trace(
+            go.Scatter(
+                x=index,
+                y=present_cycle[column],
+                mode="lines",
+                name=f"{column}  present",
+                opacity=0.7,
+            )
+        )
 
-    #--------------------------------------
+    # update layout with new title
+    comparison.update_layout(
+        title={
+            "text": "Economic cycles comparison (present vs past)",
+            "font": {"size": 24},
+        }
+    )
+
+    # --------------------------------------
     # third graph: comparison on date range
-      
+
     dff = df.copy()
 
     index = dff.index
 
-    past_cycle = dff.loc[(dff.index >  dff.index[slider[0]]) & (df.index <  dff.index[slider[1]])]
+    past_cycle = dff.loc[
+        (dff.index > dff.index[slider[0]]) & (df.index < dff.index[slider[1]])
+    ]
     present_cycle = dff.loc[(dff.index > bottoms[-2])]
-
 
     index = [i for i in past_cycle.index]
 
@@ -222,16 +263,36 @@ def update_graph(variables, cycle, slider):
         present_cycle[column] = present_cycle[column].rolling(window=45).mean()
 
         # Plotly Express
-        comparison_slider.add_trace(go.Scatter(x=index, y=past_cycle[column], mode='lines', name=f'{column} past', opacity=0.7))
-        comparison_slider.add_trace(go.Scatter(x=index, y=present_cycle[column], mode='lines', name=f'{column}  present', opacity=0.7))
-    
+        comparison_slider.add_trace(
+            go.Scatter(
+                x=index,
+                y=past_cycle[column],
+                mode="lines",
+                name=f"{column} past",
+                opacity=0.7,
+            )
+        )
+        comparison_slider.add_trace(
+            go.Scatter(
+                x=index,
+                y=present_cycle[column],
+                mode="lines",
+                name=f"{column}  present",
+                opacity=0.7,
+            )
+        )
+
     # update layout with new title
-    comparison_slider.update_layout(title={
-            'text': 'Economic cycles comparison_slider (present vs past)',
-            'font': {'size': 24}})
+    comparison_slider.update_layout(
+        title={
+            "text": "Economic cycles comparison_slider (present vs past)",
+            "font": {"size": 24},
+        }
+    )
 
     return container, hist, comparison, comparison_slider
 
+
 # ------------------------------------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
--- modules/fred_scraper.py
+++ modules/fred_scraper.py
@@ -4,40 +4,51 @@ from bs4 import BeautifulSoup
 from datetime import date
 from functools import reduce
 import numpy as np
-import time 
+import time
 import os
 from sklearn import preprocessing
 
 
-class fred_retriever():
+class fred_retriever:
     """
     Entity that scrapes fred and returns merged dataframe
     """
+
     def __init__(self):
-        
-        self.measures = {'nasdaqcom' :'NASDAQCOM', 'nyse' : 'BOGZ1FL073164003Q', 'willshire' : 'WILL5000INDFC', 
-                        'gdp':'GDP', 'gnp':'GNP', 'unem_rate' : 'UNRATE', 'interest_rate':'DFF', 
-                        'leverage':'NFCILEVERAGE', 'M2': 'M2SL', 'cpi' :'FPCPITOTLZGUSA', 
-                        'expenditures' :'PCECTPI', 'oil':'DCOILWTICO', 'core_inflation': 'CORESTICKM159SFRBATL', 
-                        'gdp_defl':'GDPDEF', 'ppi':'PPIACO', 'nnp': 'A027RC1Q027SBEA', '5y Breakeven Inflation': 'T5YIE'}
-        
-        
+        self.measures = {
+            "nasdaqcom": "NASDAQCOM",
+            "nyse": "BOGZ1FL073164003Q",
+            "willshire": "WILL5000INDFC",
+            "gdp": "GDP",
+            "gnp": "GNP",
+            "unem_rate": "UNRATE",
+            "interest_rate": "DFF",
+            "leverage": "NFCILEVERAGE",
+            "M2": "M2SL",
+            "cpi": "FPCPITOTLZGUSA",
+            "expenditures": "PCECTPI",
+            "oil": "DCOILWTICO",
+            "core_inflation": "CORESTICKM159SFRBATL",
+            "gdp_defl": "GDPDEF",
+            "ppi": "PPIACO",
+            "nnp": "A027RC1Q027SBEA",
+            "5y Breakeven Inflation": "T5YIE",
+        }
 
     def scrape_fred(self, measure):
         """
         Method to retrieve macro measures from https://fred.stlouisfed.org/series
         """
+        time.sleep(0.7)  # timeout to not overload server
+
         # get today's date
         today = str(date.today())
 
-        time.sleep(0.7)
-
         # get download url given measure and today's date
-        url = f'https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=748&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id={measure}&scale=left&cosd=1945-10-01&coed={today}&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date={today}&revision_date={today}&nd=1945-10-01'
+        url = f"https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=748&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id={measure}&scale=left&cosd=1945-10-01&coed={today}&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date={today}&revision_date={today}&nd=1945-10-01"
 
         headers = {
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
         }
 
         page = requests.get(url, headers=headers)
@@ -47,37 +58,37 @@ class fred_retriever():
         data = str(soup)
 
         # convert string to list
-        columns = [i.split(",") for i in data.split('\n')][0]
-        rows = [i.split(",") for i in data.split('\n')][1:]
+        columns = [i.split(",") for i in data.split("\n")][0]
+        rows = [i.split(",") for i in data.split("\n")][1:]
 
         # convert list to dataframe and drop last row
         df = pd.DataFrame(rows, columns=columns)
-        df.drop(df.tail(1).index,inplace=True) 
+        df.drop(df.tail(1).index, inplace=True)
 
-        df['DATE'] = pd.to_datetime(df['DATE'], utc=True)
+        # convert date column to datetime dtype
+        df["DATE"] = pd.to_datetime(df["DATE"], utc=True)
+
         return df
-
 
     def preprocess_data(self, df):
         """
         Method to clean data, interpolate nans, convert to numeric and feature engineer columns (to change)
         """
-      #  df.set_index('DATE', inplace=True)
-        
-        df= df.loc[df.index > '01-01-1971']
-        
-        df = df.replace(".", np.nan)
+        # loc data after 1971
+        df = df.loc[df.index > "01-01-1971"]
 
+        # simple preprocessing
+        df = df.replace(".", np.nan)
         df = df.apply(pd.to_numeric)
 
         df = df.interpolate()
-        df = df.interpolate(limit_direction='backward')
+        df = df.interpolate(limit_direction="backward")
 
         def convert_to_change(df):
             """
             Method to convert dataframe variable into change variables
             """
-            df1= df.copy()
+            df1 = df.copy()
             for column in df1.columns:
                 try:
                     df1[column] = df1[column].replace(".", np.nan)
@@ -89,71 +100,89 @@ class fred_retriever():
 
         df_change = convert_to_change(df)
 
-        df['GDP_change'] = df_change['GDP']
-        df['GNP_change'] = df_change['GNP']
-        df['A027RC1Q027SBEA_change'] = df_change['A027RC1Q027SBEA']
-        df['GDPDEF_change'] = df_change['GDPDEF']
+        df["GDP_change"] = df_change["GDP"]
+        df["GNP_change"] = df_change["GNP"]
+        df["A027RC1Q027SBEA_change"] = df_change["A027RC1Q027SBEA"]
+        df["GDPDEF_change"] = df_change["GDPDEF"]
 
-        df['GDP_change'].replace(0, np.nan, inplace=True)
-        df['GDP_change'].interpolate(method = 'pad', inplace=True)
-        df['GDP_change'].replace(np.nan, 0, inplace=True)
+        df["GDP_change"].replace(0, np.nan, inplace=True)
+        df["GDP_change"].interpolate(method="pad", inplace=True)
+        df["GDP_change"].replace(np.nan, 0, inplace=True)
 
-        df['GNP_change'].replace(0, np.nan, inplace=True)
-        df['GNP_change'].interpolate(method = 'pad', inplace=True)
-        df['GNP_change'].replace(np.nan, 0, inplace=True)
+        df["GNP_change"].replace(0, np.nan, inplace=True)
+        df["GNP_change"].interpolate(method="pad", inplace=True)
+        df["GNP_change"].replace(np.nan, 0, inplace=True)
 
-        df['A027RC1Q027SBEA_change'].replace(0, np.nan, inplace=True)
-        df['A027RC1Q027SBEA_change'].interpolate(method = 'pad', inplace=True)
-        df['A027RC1Q027SBEA_change'].replace(np.nan, 0, inplace=True)
+        df["A027RC1Q027SBEA_change"].replace(0, np.nan, inplace=True)
+        df["A027RC1Q027SBEA_change"].interpolate(method="pad", inplace=True)
+        df["A027RC1Q027SBEA_change"].replace(np.nan, 0, inplace=True)
 
-        df['GDPDEF_change'].replace(0, np.nan, inplace=True)
-        df['GDPDEF_change'].interpolate(method = 'pad', inplace=True)
-        df['GDPDEF_change'].replace(np.nan, 0, inplace=True)
+        df["GDPDEF_change"].replace(0, np.nan, inplace=True)
+        df["GDPDEF_change"].interpolate(method="pad", inplace=True)
+        df["GDPDEF_change"].replace(np.nan, 0, inplace=True)
 
-        df['-UNRATE'] = -df['UNRATE'] 
-        
-        df['BOGZ1FL073164003Q'] = pd.to_numeric(df['BOGZ1FL073164003Q'])
-        
+        df["-UNRATE"] = -df["UNRATE"]
+
+        df["BOGZ1FL073164003Q"] = pd.to_numeric(df["BOGZ1FL073164003Q"])
+
+        # scale data
         def scaler(df):
             x = df.values
             min_max_scaler = preprocessing.MinMaxScaler()
             x_scaled = min_max_scaler.fit_transform(x)
-            df = pd.DataFrame(x_scaled, columns = df.columns).set_index(df.index)
+            df = pd.DataFrame(x_scaled, columns=df.columns).set_index(df.index)
             return df
-        
-        df = scaler(df)
-        
 
-        df['ECOGROWTH'] = 0.7*df['-UNRATE'] + 0.7*df['GDP_change'] + 0.6*df['GNP_change'] + 0.5*df['A027RC1Q027SBEA_change'] + 0.4*df['WILL5000INDFC']
-        df['INFLATION'] = df['PCECTPI']  + df['FPCPITOTLZGUSA']  + df['NFCILEVERAGE'] + df['CORESTICKM159SFRBATL'] + df['GDPDEF_change'] + df['PPIACO'] + df['M2SL']
+        df = scaler(df)
+
+        df["ECOGROWTH"] = (
+            0.7 * df["-UNRATE"]
+            + 0.7 * df["GDP_change"]
+            + 0.6 * df["GNP_change"]
+            + 0.5 * df["A027RC1Q027SBEA_change"]
+            + 0.4 * df["WILL5000INDFC"]
+        )
+        df["INFLATION"] = (
+            df["PCECTPI"]
+            + df["FPCPITOTLZGUSA"]
+            + df["NFCILEVERAGE"]
+            + df["CORESTICKM159SFRBATL"]
+            + df["GDPDEF_change"]
+            + df["PPIACO"]
+            + df["M2SL"]
+        )
 
         return df
-    
-    
-    def merge(self):
+
+    def run(self):
         """
         Method to merge all the datasets
         """
-        # retrieve all the measures 
-        dataframes=[]
+        # retrieve all the measures
+        dataframes = []
         for i in range(0, len(self.measures)):
             name = list(self.measures.keys())[i]
             value = list(self.measures.values())[i]
-            locals()[name] = self.scrape_fred(f"{value}") 
+            locals()[name] = self.scrape_fred(f"{value}")
             dataframes.append(locals()[name])
 
+        # merge dataframes
+        data = reduce(
+            lambda left, right: pd.merge_asof(
+                left.sort_values("DATE"),
+                right.sort_values("DATE"),
+                left_on="DATE",
+                right_on="DATE",
+                direction="backward",
+            ),
+            dataframes,
+        ).set_index("DATE")
 
-        data = reduce(lambda  left,right: pd.merge_asof(left.sort_values('DATE'), 
-                        right.sort_values('DATE'),
-                        left_on='DATE',
-                        right_on='DATE',
-                        direction='backward'), dataframes).set_index('DATE')
-        
+        # preprocess merged dataframe
         data = self.preprocess_data(data)
         return data
 
 
 my_instance = fred_retriever()
-data = my_instance.merge()
-data.to_csv(os.getcwd() + '/data.csv')
-
+data = my_instance.run()
+data.to_csv(os.getcwd() + "/data.csv")
